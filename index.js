27c03e2428d00c89d26b9f1666be55dd725ce41e
mina-y-Khalil <mina.y.khalil90@gmail.com>
2025-09-06 20:21:24 -0700

    Adding Event Listeners to a Button and some styling

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+
+
+
 const buttons = document.querySelectorAll(".drum");
 
 for (const button of buttons) {
     button.addEventListener('click', ()=> {
-        alert("I got clicked!")
+        // alert("I got clicked!")
+        button.style.color = "teal";
+        
+        
     })
 }
+
+
+
+
+
+// let audio = new Audio(`sounds/tom-1.mp3`);
+// audio.play();
