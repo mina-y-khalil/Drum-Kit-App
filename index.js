1b77e8de18b5483be31d019fac3c47c480263b8d
mina-y-Khalil <mina.y.khalil90@gmail.com>
2025-09-06 21:51:00 -0700

    handle key parameter properly for button clicks and keyboard events

--- index.js
+++ index.js
@@ -4,52 +4,59 @@
 const buttons = document.querySelectorAll(".drum");
 
 for (const button of buttons) {
-    button.addEventListener('click', ()=> {
-        // alert("I got clicked!")
+    button.addEventListener('click', ()=> {       
         const key = button.innerHTML;
-
-
-    // will use the switch statement to play different sounds based on the button clicked instead of using if-else statements.
-    
-        switch (key) {
-            case "w":
-                let audio = new Audio(`sounds/tom-1.mp3`);
-                audio.play();
-                
-                break; // this tells the program to exit the switch statement and continue executing the code that follows it.
-            case "a":
-                let audio1 = new Audio(`sounds/tom-2.mp3`);
-                audio1.play();
-                break;
-            case "s":
-                let audio2 = new Audio(`sounds/tom-3.mp3`);
-                audio2.play();
-                break;
-            case "d":
-                let audio3 = new Audio(`sounds/tom-4.mp3`);
-                audio3.play();
-                break;
-            case "j":
-                let audio4 = new Audio(`sounds/crash.mp3`);
-                audio4.play();
-                break;
-            case "k":
-                let audio5 = new Audio(`sounds/snare.mp3`);
-                audio5.play();
-                break;
-            case "l":
-                let audio6 = new Audio(`sounds/kick-bass.mp3`);
-                audio6.play();
-                break;
-        
-            default:
-                break;
-        }
-        
+        makeSound(key);
         
     })
 }
 
+// Detecting keyboard press
+document.addEventListener('keydown', (event) => {
+    makeSound(event.key);
+});
+
+
+function makeSound (key) {
+        // will use the switch statement to play different sounds based on the button clicked instead of using if-else statements.
+
+    switch (key) {
+        case "w":
+            let tom1 = new Audio(`sounds/tom-1.mp3`);
+            tom1.play();
+            
+            
+            break; // this tells the program to exit the switch statement and continue executing the code that follows it.
+        case "a":
+            let tom2 = new Audio(`sounds/tom-2.mp3`);
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio(`sounds/tom-3.mp3`);
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio(`sounds/tom-4.mp3`);
+            tom4.play();
+            break;
+        case "j":
+            let crash = new Audio(`sounds/crash.mp3`);
+            crash.play();
+            break;
+        case "k":
+            let kickBass = new Audio(`sounds/kick-bass.mp3`);
+             kickBass.play();
+            break;
+        case "l":
+            let snare = new Audio(`sounds/snare.mp3`);
+            snare.play();
+            break;
+    
+        default: // this is like else in the if statement
+            break;
+    }
+    
+}
 
 
 
